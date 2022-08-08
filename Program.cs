17df47e91769cef3f434a63de5151830072680ef
KasperMR <kaspermoeberg@gmail.com>
2022-08-08 15:51:06 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int myAge = 29;
+string myName = "Kasper M. Rasmussen";
+Console.WriteLine("Hello world i am " + myName + " and i am " + myAge + " years old");
